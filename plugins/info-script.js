a5cec1ddc442b6d030945d7367a6ba5e027a6491
Vamses <108464869+VamsesOfficial@users.noreply.github.com>
2022-10-23 16:49:01 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,12 +4,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://chat.whatsapp.com/DbbDlICeGFuJiX9KlFyI17Q*`
+let str = `*https://chat.whatsapp.com/JxI53XkvnnYHP2D1eIZ8fu*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Cok', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://chat.whatsapp.com/DbbDlICeGFuJiX9KlFyI17",
-    title: 'Makima-MultiDevice',
+    mediaUrl: "https://chat.whatsapp.com/JxI53XkvnnYHP2D1eIZ8fu",
+    title: 'Chonix',
     body: 'Cari Aja Di Situ',
     thumbnail: thumb,
     sourceUrl: sgc
